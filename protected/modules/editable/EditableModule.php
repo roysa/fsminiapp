3b96b202d43f8c1bed33712f4191131ff05fd640
Dmitry Sylkin <roysa@valant.com.ua>
2014-09-01 19:57:46 +0300

    ok

--- protected/modules/editable/EditableModule.php
+++ protected/modules/editable/EditableModule.php
@@ -8,11 +8,33 @@ class EditableModule extends CModule
     {
         $script = "$(document).ready(function(){ 
             Aloha.ready(function() {
-            console.log('Setting Aloha for ' + '{$selector}');
-            Aloha.jQuery('{$selector}').aloha();
+            var el = Aloha.jQuery('{$selector}');
+            el.aloha();
+            
+            var changePending = false;
+
+            function elChanged()
+            {
+                if (changePending) {
+                    clearTimeout(changePending);
+                }
+                changePending = setTimeout(function(){
+                    console.log('SAVE!');
+                }, 1300);
+            }
+
+            el.bind('keydown', function(event) {
+                elChanged();
+            });
+            el.bind('click', function(event) {
+                elChanged();
+            });
+            el.bind('blur', function(event) {
+                elChanged();
+            });
         });"
             . "});";
-        FSMiniApp::app()->script->registerScript($script);
+        FSMiniApp::app()->script->registerScript('editable-' . $selector, $script);
     }
     
     public function init($config)
@@ -25,6 +47,7 @@ class EditableModule extends CModule
         
         FSMiniApp::app()->registerScriptFile($dir . '/lib/aloha.js', null, array('data-aloha-plugins' => 'common/ui,
 								common/format,
+								common/align,
 		                        common/table,
 		                        common/list,
 		                        common/link,
@@ -35,7 +58,7 @@ class EditableModule extends CModule
 		                        common/contenthandler,
 		                        common/paste,
 		                        common/commands,
-		                        common/abbr'));
+                                        extra/videoblock'));
         FSMiniApp::app()->registerCssFile($dir . '/css/aloha.css');
         parent::init($config);
     }
